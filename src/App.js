14f035820ce644253aa58311ec6f06ae33a0f93e
Bogdan Bogdanov <Bogdano4653@gmail.com>
2021-03-10 20:32:02 +0200

    Add MainLayout in App and paste dummy data in that layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Provider } from "react-redux";
+import MainLayout from "./layouts/MainLayout";
 import store from "./store";
 function App() {
-  return <Provider store={store}></Provider>;
+  return (
+    <Provider store={store}>
+      <MainLayout>{"Hello"}</MainLayout>
+    </Provider>
+  );
 }
 
 export default App;
