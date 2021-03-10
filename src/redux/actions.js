f812f05266f9d40224a5e62b44592e3831a70cb5
Bogdan Bogdanov <Bogdano4653@gmail.com>
2021-03-10 22:00:59 +0200

    Add DELETE_TASK_TIMER Action,  and id for each timeres

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,11 +1,21 @@
-import { ADD_TASK_TIMER } from "./actionTypes";
+import { ADD_TASK_TIMER, DELETE_TASK_TIMER } from "./actionTypes";
 
-export const addTimer = (title, startingTime) => {
+export const addTimer = (id, title, startingTime) => {
   return {
     type: ADD_TASK_TIMER,
     payload: {
+      id,
       title,
       startingTime,
+    },
+  };
+};
+
+export const deleteTimer = (id) => {
+  return {
+    type: DELETE_TASK_TIMER,
+    payload: {
+      id,
     },
   };
 };
