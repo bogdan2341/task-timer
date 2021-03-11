0f46c9148b09f3bacfd4518a8027a03596f773eb
Bogdan <bogdanov4653@gmail.com>
2021-03-12 00:31:27 +0200

    Add KeyDownHandler

--- src/components/InputNewTask.js
+++ src/components/InputNewTask.js
@@ -2,18 +2,21 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTimer } from "../redux/actions";
 import { Button, makeStyles, TextField } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import moment from "moment";
 
 const genUID = () => Math.random().toString(36).substr(2, 9);
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    marginTop: theme.spacing(4),
-    padding: theme.spacing(2),
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(3),
     display: "flex",
     justifyContent: "space-between",
   },
   textField: {
-    width: "60%",
+    flexGrow: 2,
+    marginRight: theme.spacing(1),
   },
 }));
 
@@ -21,21 +24,32 @@ function InputNewTask() {
   const [tittle, setTittle] = useState("");
   const dispatch = useDispatch();
   const classes = useStyles();
+
+  const keyDownHandler = (event) => {
+    if (event.key === "Enter") {
+      saveTimerHandler();
+    }
+  };
+
+  const saveTimerHandler = () => {
+    const taskTittle = tittle || `Task#[${moment().format("HH:mm:ss")}]`;
+    setTittle("");
+    return dispatch(addTimer(genUID(), taskTittle, Date.now()));
+  };
+
   return (
     <div className={classes.root}>
       <TextField
+        variant="outlined"
         className={classes.textField}
         type="text"
         onChange={(e) => setTittle(e.target.value)}
         value={tittle}
         label="Task title"
+        onKeyPress={keyDownHandler}
       />
-      <Button
-        onClick={() => dispatch(addTimer(genUID(), tittle, 2323))}
-        variant="outlined"
-        color="primary"
-      >
-        Add task
+      <Button onClick={saveTimerHandler} variant="contained" color="primary">
+        <AddIcon />
       </Button>
     </div>
   );
