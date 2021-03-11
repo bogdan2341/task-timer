8b2af1388d7a4d392b78bcf32c786746db281f21
Bogdan <bogdanov4653@gmail.com>
2021-03-12 00:35:05 +0200

    Implements new actions in reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,11 @@
-import { ADD_TASK_TIMER, DELETE_TASK_TIMER, TOGGLE_PAUSE } from "./actionTypes";
+import {
+  ADD_TASK_TIMER,
+  DELETE_TASK_TIMER,
+  PAUSE_TIMER,
+  RESUME_TIMER,
+  SET_TIMERS,
+  TOGGLE_PAUSE,
+} from "./actionTypes";
 
 const initialState = {
   timers: [],
@@ -7,22 +14,48 @@ const initialState = {
 function reducer(state = initialState, { payload, type }) {
   switch (type) {
     case ADD_TASK_TIMER:
-      console.log({ timers: [...state.timers, payload] });
       return {
         ...state,
-        timers: [...state.timers, payload],
+        timers: [payload, ...state.timers],
       };
     case DELETE_TASK_TIMER:
       return {
         ...state,
         timers: state.timers.filter((t) => t.id !== payload.id),
       };
-    case TOGGLE_PAUSE:
+    case RESUME_TIMER:
+      console.log(state);
       return {
         ...state,
-        timers: state.timers.map((t) =>
-          t.id === payload.id ? { ...t, isPaused: !t.isPaused } : t
-        ),
+        timers: state.timers.map((t) => {
+          if (t.id === payload.id) {
+            return {
+              ...t,
+              isPaused: false,
+              pausingTime: t.pausingTime,
+              startingTime: t.startingTime + (payload.time - t.pausingTime),
+            };
+          } else {
+            return t;
+          }
+        }),
+      };
+    case PAUSE_TIMER:
+      console.log(state);
+      return {
+        ...state,
+        timers: state.timers.map((t) => {
+          if (t.id === payload.id) {
+            return { ...t, isPaused: true, pausingTime: payload.time };
+          } else {
+            return t;
+          }
+        }),
+      };
+    case SET_TIMERS:
+      return {
+        ...state,
+        timers: payload.timersArray,
       };
     default:
       return state;
