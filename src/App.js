640e3a79c91b4603c7fd40740a52c48ba3c54f8e
Bogdan <bogdanov4653@gmail.com>
2021-03-12 11:49:24 +0200

    Add theme in project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { Provider } from "react-redux";
 import InputNewTask from "./components/InputNewTask";
 import TaskList from "./components/TaskList";
 import MainLayout from "./layouts/MainLayout";
 import store from "./store";
+import theme from "./theme";
 function App() {
   return (
     <Provider store={store}>
-      <CssBaseline />
-      <MainLayout>
-        <InputNewTask />
-        <TaskList />
-      </MainLayout>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MainLayout>
+          <InputNewTask />
+          <TaskList />
+        </MainLayout>
+      </ThemeProvider>
     </Provider>
   );
 }
